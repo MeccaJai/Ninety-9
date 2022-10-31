6eb8ccec98f94c5e01ab419058f8f02fe8b8d941
Caleb <57421085+cyoung712@users.noreply.github.com>
2022-10-31 15:56:03 -0400

    Update AdminRoute.js for auth

--- ninety-9/src/components/AdminRoute.js
+++ ninety-9/src/components/AdminRoute.js
@@ -1,14 +1,13 @@
 import React, {useContext} from 'react';
 import {AuthContext} from "../context/AuthContext";
+import {Navigate} from "react-router-dom";
 
-function AdminRoute()
+function AdminRoute({children})
 {
     // const {currentUser} = useContext(AuthContext);
 
     let localStorageItem = localStorage.getItem("user");
 
-    return (JSON.parse(localStorageItem).Account_Type === "A");
-
+    return (JSON.parse(localStorageItem).Account_Type === "A") ? children : <Navigate to={"/login"} />;
 }
-
 export default AdminRoute;
