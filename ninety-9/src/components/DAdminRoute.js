9bd57db05abd7872ca9fc430797efc87161e4be3
Caleb <57421085+cyoung712@users.noreply.github.com>
2022-10-31 15:55:02 -0400

    Update DAdminRoute.js for auth

--- ninety-9/src/components/DAdminRoute.js
+++ ninety-9/src/components/DAdminRoute.js
@@ -1,12 +1,13 @@
 import React, {useContext} from 'react';
 import {AuthContext} from "../context/AuthContext";
+import {Navigate} from "react-router-dom";
 
-function DAdminRoute()
+function DAdminRoute({children})
 {
     // const {currentUser} = useContext(AuthContext);
 
     let localStorageItem = localStorage.getItem("user");
 
-    return (JSON.parse(localStorageItem).Account_Type === "D");
+    return (JSON.parse(localStorageItem).Account_Type === "D") ? children : <Navigate to={"/login"} />;
 }
 export default DAdminRoute;
