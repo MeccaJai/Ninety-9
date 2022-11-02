2b0c7e0caa9fb3ae4d63250b8b3bf2261b3b73c5
Caleb <57421085+cyoung712@users.noreply.github.com>
2022-11-02 16:34:48 -0400

    Add files via upload

--- ninety-9/src/components/UserCard.js
+++ ninety-9/src/components/UserCard.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import '../App.css';
+import {initializeApp} from "firebase/app";
+import {collection, getFirestore, orderBy, query, where, doc, setDoc, updateDoc} from "firebase/firestore";
+
+async function Deactivate(props) {
+    const firebaseConfig = {
+        apiKey: "AIzaSyD0O_zMoKyV3Bwrc_QZEftuy14VWyGMadA",
+        authDomain: "bar4bar-62819.firebaseapp.com",
+        projectId: "bar4bar-62819",
+        storageBucket: "bar4bar-62819.appspot.com",
+        messagingSenderId: "108693622175",
+        appId: "1:108693622175:web:2d2464fa1239e0829281aa",
+        measurementId: "G-0RX853S9GS"
+    };
+
+    const user = props.user;
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+
+    const accountRef = doc(db, "Accounts", user.id);
+    await updateDoc(accountRef, {
+        isActive: false
+    });
+
+    if(user.isActive == false){
+        console.log("Successfully deactivated account...");
+    }
+}
+
 
 function UserCard(props) {
-	return (
-		<>
-		    <div className="offset-md-2 col-md-8 mb-3" >
+    return (
+        <>
+            <div className="offset-md-2 col-md-8 mb-3" >
                 <br/>
                 <div className="card h-100 position-relative userMar ">
                     <div className="row g-0">
@@ -27,8 +55,8 @@ function UserCard(props) {
                     </div>
                 </div>
             </div>
-		</>
-	);
+        </>
+    );
 }
 
 export default UserCard;
