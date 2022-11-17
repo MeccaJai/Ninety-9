ff2556125bb005be0549ce76bdff6f042a77913a
smileydey <89495809+smileydey@users.noreply.github.com>
2022-11-16 20:05:03 -0500

    Add files via upload
    the navigation bar. i did not style it but if you want i can

---
+++ ninety-9/src/components/TabBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+function TabBar (){
+    return(
+        <ul className="nav nav-tabs justify-content-center">
+
+        <li className="nav-item">
+            <Link to="/home" className="nav-link active" aria-current="page" aria-expanded="false"> Home </Link>
+        </li>
+
+        <li className="nav-item">
+            <Link to="/search" className="nav-link active" aria-current="page"
+                  aria-expanded="false"> Search </Link>
+        </li>
+
+        <li className="nav-item">
+            <Link to="/logout" className="nav-link active" aria-current="page"
+                 aria-expanded="false"> Sign Out </Link>
+        </li>
+    </ul>
+    )
+
+
+}
+
+export default TabBar;
